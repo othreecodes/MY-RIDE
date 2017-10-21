077611f3ca589e1ad5e7c8378974c66e223b2b90
Obi Uchenna David <daviduchenna@outlook.com>
2017-10-21 18:00:06 +0100

    changed DEBUG to False

--- Carpool/settings.py
+++ Carpool/settings.py
@@ -52,10 +52,10 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE_CLASSES = [
-  'corsheaders.middleware.CorsMiddleware',
-  'django.middleware.common.CommonMiddleware',
   'django.middleware.security.SecurityMiddleware',
   'whitenoise.middleware.WhiteNoiseMiddleware',
+  'corsheaders.middleware.CorsMiddleware',
+  'django.middleware.common.CommonMiddleware',
   'django.contrib.sessions.middleware.SessionMiddleware',
   'django.middleware.csrf.CsrfViewMiddleware',
   'django.contrib.auth.middleware.AuthenticationMiddleware',
