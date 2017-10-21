f443a2439c2efb69911771ca616ee1c90f6d50aa
Obi Uchenna David <daviduchenna@outlook.com>
2017-10-21 17:39:04 +0100

    static file config

--- Carpool/urls.py
+++ Carpool/urls.py
@@ -5,6 +5,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 import notifications.urls
 from os import path
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     url(r'^admin/', include('smuggler.urls')),
@@ -19,7 +20,7 @@ urlpatterns = [
 ]
 
 
-urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+# urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
-
+urlpatterns += staticfiles_urlpatterns()
